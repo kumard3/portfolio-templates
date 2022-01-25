68079fe042daf2993c20821976382ecec314e318
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-25 23:21:14 +0530

    changes made

--- portfolio-template-2/pages/index.tsx
+++ portfolio-template-2/pages/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <>
     <div className="bg-black min-h-screen flex flex-col justify-center items-center w-full ">
-      <Main />
+      <Section />
       <h1 className="text-7xl my-10 font-bold ">Welcome</h1>
       <p className="text-3xl font-bold ">
         Next js Particles js Starter with custom image 
