75ab9b9985acad2602b0f97496b4c56459182747
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-26 21:34:55 +0530

    changes made to template 3

---
+++ portfolio-template3/components/data.js
@@ -0,0 +1,26 @@
+export const data = {
+  initial: "LI",
+  resumeUrl: "#",
+  name: "Lorem Ipsum",
+  position: "Software developer",
+  projectsList: [
+    {
+      id: 1,
+      name: "Lorem Ipsum",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad facilis labore assumenda vero amet modi sapiente quis minus! Quisquam animi ab quam aliquid perspiciatis maxime nam deleniti, recusandae velit similique.",
+      image: "/images/large.jpeg",
+      mobileImage: "/images/small.jpeg",
+      link: "#",
+      GitHub: "#",
+      tools: ["Next.js", "Tailwind CSS", "p5.js"],
+    },
+  ],
+  seo: {
+      description: "lorem ipsum",
+      url: "lorem.com",
+      image: "/large.jpeg",
+      title: "Lorem Ipsum",
+      favicon : "/favicon.ico"
+  }
+};
